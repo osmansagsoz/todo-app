b1298fd01dce6e34db9e1e94cc9df98c60522c33
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-03-23 22:38:44 +0300

    add functions

--- todo.js
+++ todo.js
@@ -1,6 +1,10 @@
 const todoForm = document.querySelector('.js-form');
 const todoList = document.querySelector('.todo-list');
 const input = document.querySelector('#todo');
+const doneList = document.querySelector('.todo-list-done');
+const emptyState = document.querySelector('.empty-state');
+const subHeader = document.querySelector('.sub');
+const subP = document.querySelector('.subP');
 
 let todos = [];
 
@@ -32,13 +36,15 @@ function renderTodo(todo) {
     const button = document.createElement('button');
     li.dataset.todoid = todo.id;
     input.type = 'radio';
-    input.value = todo.name;
+    // input.value = todo.name;
     input.id = todo.id;
-    input.name = todo.id;
+    // input.name = todo.id;
     label.htmlFor = todo.id;
     label.textContent = todo.name;
+    label.value = todo.id;
     button.ariaLabel = 'remove';
     button.className = 'remove';
+    button.value = todo.id;
     button.textContent = 'x';
     li.appendChild(input);
     li.appendChild(label);
@@ -46,15 +52,47 @@ function renderTodo(todo) {
     todoList.appendChild(li);
 }
 
+function markAsComplete(id) {
+   
+    const todoRef = todos.find(todo => todo.id === id);
+    todoRef.complete = !todoRef.complete;
+    
+    const doneTodo = document.querySelector(`li[data-todoid='${id}']`);
+    doneTodo.remove();
+    doneList.appendChild(doneTodo);
+    emptyState.classList.add('empty');
+    // todoList.dispatchEvent(new CustomEvent('todosUpdated'));
+  }
+
 function deleteTodo(todoId) {
     const deleteLi = document.querySelector(`li[data-todoid='${todoId}']`);
-    console.log('deleting', deleteLi);
-
     deleteLi.remove();
 
     todos = todos.filter((todo) => todoId !== todo.id);
-    
+    if (todos.length === 0) {
+       emptyState.classList.remove('empty');
+       subHeader.textContent = 'She done already done had herses!';
+        subP.textContent = '';
+     }
 }
 
 todoForm.addEventListener('submit', addTodo);
+
+doneList.addEventListener('click', function(e) {
+    if (e.target.matches('button')) {
+        deleteTodo(e.target.value);
+      }
+});
+
+todoList.addEventListener('click', function(e) {
+    if (e.target.matches('button')) {
+        deleteTodo(e.target.value);
+      }
+});
+
+todoList.addEventListener('click', function(e) {
+    if (e.target.matches('input[type=radio] + label')) {
+        markAsComplete(e.target.value);
+      }
+  });
 
