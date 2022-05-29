3b003e0c26d15668980c3e1f7a04080fc4974134
Osman Sagsoz <osmansagsoz1@gmail.com>
2022-05-29 20:49:35 +0300

    removal

--- todoist.js
+++ todoist.js
@@ -1,6 +1,6 @@
 // $ curl -X GET \
 //   https://api.todoist.com/rest/v1/projects \
-//   -H "Authorization: Bearer 77d5cbd9bfbf9f5c1e77d0facf68f8cb0935396e"
+//   -H "Authorization: Bearer token"
 
 //  export async function geetTodo() {
 //     const endpoint = await fetch('https://api.todoist.com/rest/v1/tasks', {
